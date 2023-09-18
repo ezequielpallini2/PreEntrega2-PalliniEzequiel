2a13f6c0095d27cd6498f01cac7e59b9234f6cf4
Ezequiel Pallini <ezequielpallini2@gmail.com>
2023-09-17 22:48:55 -0300

    Añadida busqueda, falta pulirla

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,6 +1,32 @@
 import React from 'react'
 import { Item } from './Item'
 
+import Filter from "../assets/Filter"
+
+const productsFiltered = ({ products, filterState, handleFilterChange }) => (
+    
+        <>
+            <div className="w-25">
+                <label>Ingrese el producto que quiere buscar</label>            
+                <input className="form-control" type="text" value='' onChange={handleFilterChange} />
+            </div>
+            <h1>Productos</h1>
+            { 
+                filterState === '' ? 
+                    products.map(product => <Item key={product.id} product={product} /> )  
+                : 
+                    products
+                        .filter(product => product.name.toLowerCase().includes(filterState.toLowerCase()))
+                        .map(product => <Item key={product.id} product={product} /> )  
+            }
+            
+        </>
+
+)
+
+
+
+
 const ItemList = ({eventos}) => {
 
   return (
@@ -8,8 +34,10 @@ const ItemList = ({eventos}) => {
         <h2></h2>
         <div className="container" id="container">
             
-            {eventos.map((event) => <Item evento={event} key={event.id} />)
-             }
+             {eventos.map((event) => <Item evento={event} key={event.id} />)             }
+             {/*  <Filter evento={eventos}>
+                { productsFiltered }
+            </Filter>*/}
 
         </div>
 
